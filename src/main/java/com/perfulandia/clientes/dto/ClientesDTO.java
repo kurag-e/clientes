6b578e99cbe4abe5f67a6a6781390de25a05f5db
darlynee zamorano <darl.zamorano@duocuc.cl>
2025-07-02 00:42:45 -0400

    s

--- src/main/java/com/perfulandia/clientes/dto/ClientesDTO.java
+++ src/main/java/com/perfulandia/clientes/dto/ClientesDTO.java
@@ -1,13 +1,13 @@
 package com.perfulandia.clientes.dto;
 
-
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
-import com.fasterxml.jackson.annotation.JsonInclude;
-
 
 @Data
+@Relation(collectionRelation = "clientesDTOList")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ClientesDTO extends RepresentationModel<ClientesDTO> {
     private Integer idCliente;
     private Integer idUsuario;
